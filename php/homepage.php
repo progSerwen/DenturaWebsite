4c2c7e4ec85518c5b5b85410a8360e440fc00405
Shadee <121048030+Ceeda02@users.noreply.github.com>
2024-10-11 12:34:54 +0800

    Update homepage.php

--- php/homepage.php
+++ php/homepage.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dentura</title>
+    <title>Denture Ni Ano</title>
     <link rel="icon" href="/assets/images/dlogo.ico" type="image/x-icon">
     
     <!-- Bootstrap CSS -->
